4015dc9c825a1a0339a5b261424bc4a2e34b531a
Mau McCoy <mccoymauricio96@gmail.com>
2020-08-14 20:30:58 -0500

    $message

---
+++ gitBasic.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#This program meant to:
+# git init
+# git add .
+# git commit -m ''
+# git push origin master
+#All in one command in order to save time while pushing changes to gitHub.
+
+git init
+
+git add .
+
+echo "Commit message: "
+
+read message
+
+git commit -m '$message'
+
+git push -f origin master
