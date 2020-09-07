e4c6a7515697d299aa6426abb778e928d9227f7e
Mau McCoy <mccoymauricio96@gmail.com>
2020-09-07 11:59:10 -0500

    small font wins

--- gitBasic.sh
+++ gitBasic.sh
@@ -13,7 +13,7 @@ git init
 
 git add .
 
-figlet -f small "Commit message:"
+figlet -f mini "Commit message:"
 
 read message
 
@@ -21,4 +21,4 @@ git commit -m "$message"
 
 git push -f origin master
 
-figlet -f small "Upload Succesful"
+figlet -f mini "Upload Succesful"
