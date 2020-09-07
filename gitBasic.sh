026c28a306fb5bdd909d5c55c0e531c0564566af
Mau McCoy <mccoymauricio96@gmail.com>
2020-09-07 11:51:33 -0500

    i still dont like this font

--- gitBasic.sh
+++ gitBasic.sh
@@ -13,7 +13,7 @@ git init
 
 git add .
 
-figlet -f larry3d "\033[1;31mCommit message:\033[0m"
+figlet -f larry3d "Commit message:"
 
 read message
 
@@ -21,4 +21,4 @@ git commit -m "$message"
 
 git push -f origin master
 
-figlet -f larry3d "\033[1;92mEverything has been succefully commited\033[0m"
+figlet -f larry3d "Upload Succesful"
